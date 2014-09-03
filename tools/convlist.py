c40572a62ec36480f524ee221ab2f83c35ec4fc7
jj4jj <resc@vip.qq.com>
2014-09-03 13:44:09 +0800

    modify tools

--- tools/convlist.py
+++ tools/convlist.py
@@ -10,8 +10,6 @@ sys.setdefaultencoding("utf-8")
 ############################################################################
 def writeXlsToBin(tableObj,xlsName,binDataName,titleMap):
 	xlsData = xlrd.open_workbook(xlsName)
-	for k in titleMap.keys():
-		print(k,titleMap[k])		
 	for table in xlsData.sheets():
 		for row in range(table.nrows):
 			if row > 0:
@@ -20,12 +18,10 @@ def writeXlsToBin(tableObj,xlsName,binDataName,titleMap):
 				if row == 0:
 					continue
 				else:
-					print(table.cell(0,col).value)
 					attrName = titleMap[table.cell(0,col).value]
 					attrType = type(getattr(entry,attrName))
 					attrValue = (attrType)(table.cell(row,col).value)
 					setattr(entry,attrName,attrValue)
-					print('set attr',attrName,'=',attrValue)
 	f=open(binDataName,"wb")
 	f.write(tableObj.SerializeToString())
 	f.close()
@@ -36,11 +32,20 @@ def readBinToObj(tableObj,binDataName):
 	tableObj.ParseFromString(f.read())
 	f.close()
 #############################################################################
-
+#import re
 def printObj(tableObj):
+	it = 0
 	for entry in tableObj.list:
-		for attr in dir(entry):
-			print(getattr(entry,attr))
+		entry_string=""
+		if 0 == it:
+			for attr in entry.ListFields():
+				entry_string += str(attr[0].name) + '\t'
+			print(entry_string)
+			it=1
+		entry_string=""
+		for attr in entry.ListFields():
+			entry_string += str(getattr(entry,str(attr[0].name))) + '\t'
+		print(entry_string)
 
 
 #############################################################################
@@ -59,10 +64,12 @@ def printObj(tableObj):
 import	demo_pb2
 ######################################
 demotable = demo_pb2.DemoTable()
+readBinToObj(demotable,'demo.bin')
+printObj(demotable)
 demoTitleMap={}
-demoTitleMap[U'ID']='id'
-demoTitleMap[U'名字']='name'
-writeXlsToBin(demotable,'demo.xlsx','demo.bin',demoTitleMap);
+#demoTitleMap[u'ID']='id'
+#demoTitleMap[u'']='name'
+#writeXlsToBin(demotable,'demo.xlsx','demo.bin',demoTitleMap);
 
 
 
