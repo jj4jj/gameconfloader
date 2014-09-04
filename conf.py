6230bdb3190645f51cce97c0898d8273747189b8
jj4jj <resc@vip.qq.com>
2014-09-04 16:25:38 +0800

    display info

--- conf.py
+++ conf.py
@@ -1,7 +1,12 @@
-
-
-convTables={}
-convDemo={
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+#############################################################################################3
+convTableList=(
+#################custom conv define#########################################
+{
 	'name':ur'示例显示表',#display name
 	'metaName':'Demo',#meta name
 	'metaFile':'demo.desc',#meta name
@@ -10,27 +15,10 @@ convDemo={
 	'count':100, #default is 16
 	'sort':'asc',#default is asc
 	'group':1,#default is 1
-};
+},
 
+#################custom conv define#########################################
+)
 
-def AddTable(conv):
-	convTables[conv.name]=conv
-end
-
-#--require "conf.lua"
-AddTable(convDemo)
-
-
-############################################
-for k in convTables.keys():
-	tips = "conv talbe "+convTables[k]+" : "+convTable[k].metaName+" from "+metaFile+' and '+convTable[k]..xls+" to "+convTable[k].bin
-	print(tips)	
-	execString = 'python convlist.py meta/'+convTable[k].metaFile+' '+
-		convTable[k].metaName+' xls/'+
-		convTable[k].xls+' data/'+convTable[k].bin
-	print(execString)
-	#python convlist.py meta/demo_desc.py Demo xls/demo.xlsx data/demo.bin
-	#call python script generate proto 
-	#call protobuff genetae python or cpp gen
-	#call python script genetate bin from xls with proto
-
+def GetConvertTableList():
+	return convTableList
--- gameconfcvt.py
+++ gameconfcvt.py
@@ -4,27 +4,20 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 convTables={}
-convDemo={
-	'name':ur'示例显示表',#display name
-	'metaName':'Demo',#meta name
-	'metaFile':'demo.desc',#meta name
-	'xls':'demo.xls',#default name is same with meta 
-	'bin':'demo.bin',#default name is same with meta
-	'count':100, #default is 16
-	'sort':'asc',#default is asc
-	'group':1,#default is 1
-};
+
 def AddTable(conv):
+	print('Add Table = '+conv['name']+' ( Meta = '+conv['metaName']+' , MetaFile = '+conv['metaFile']+' , XlsFile = '+conv['xls']+' , BinFile = '+conv['bin']+' )');
 	convTables[conv['name']]=conv
 
 #--require "conf.lua"
-AddTable(convDemo)
-
-
+#for demo
+#AddTable(convDemo)
+############################################
+ct=__import__('conf')
+for cv in ct.GetConvertTableList():
+	AddTable(cv)
 ############################################
 for k in convTables.keys():
-	tips = "conv talbe "+convTables[k]['name']+" : "+convTables[k]['metaName']+" from "+convTables[k]['metaFile']+' and '+convTables[k]['xls']+" to "+convTables[k]['bin']
-	print(tips)	
 	execString = 'python convlist.py meta/'+convTables[k]['metaFile']+' '+\
 		convTables[k]['metaName']+' xls/'+\
 		convTables[k]['xls']+' data/'+convTables[k]['bin']
